0215907b77f73c680be2aee89f6d018a6a71e90c
Ohgar <orelliholliday@gmail.com>
2025-08-02 17:07:47 -0400

    new build + removed dep build

--- Thorn Survivors Web/ServiceWorker.js
+++ Thorn Survivors Web/ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "Orelli H-G-Thorn Survivors-0.2";
-const contentToCache = [
-    "Build/Thorn Survivors Web.loader.js",
-    "Build/Thorn Survivors Web.framework.js.br",
-    "Build/Thorn Survivors Web.data.br",
-    "Build/Thorn Survivors Web.wasm.br",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
