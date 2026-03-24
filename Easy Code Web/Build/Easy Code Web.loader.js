174a342b89e3e261c16bc99cd33411bb96239f62
Orelli Holliday-Garcia <121402866+Ohgar@users.noreply.github.com>
2026-03-24 12:57:53 -0400

    mouse -> eventData

--- Easy Code Web/Build/Easy Code Web.loader.js
+++ Easy Code Web/Build/Easy Code Web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57926115,
+    wasmFileSize: 57926005,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
