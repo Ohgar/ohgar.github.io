1574fe33728c900df7c55697630d07af713464a9
Orelli Holliday-Garcia <121402866+Ohgar@users.noreply.github.com>
2026-03-23 20:25:03 -0400

    touch?

--- Easy Code Web/Build/Easy Code Web.loader.js
+++ Easy Code Web/Build/Easy Code Web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57926865,
+    wasmFileSize: 57926786,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
