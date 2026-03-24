0e01931f9d3b911d13bf845b8faa251607d81b03
Orelli Holliday-Garcia <121402866+Ohgar@users.noreply.github.com>
2026-03-24 12:23:03 -0400

    removed cursor object

--- Easy Code Web/Build/Easy Code Web.loader.js
+++ Easy Code Web/Build/Easy Code Web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57926870,
+    wasmFileSize: 57926190,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
