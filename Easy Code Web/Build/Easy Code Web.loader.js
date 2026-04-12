8f64e533cdfa36cb4fcf83a3c01a2c1b2ea04f52
Orelli Holliday-Garcia <121402866+Ohgar@users.noreply.github.com>
2026-04-12 17:24:12 -0400

    updated code button behavior

--- Easy Code Web/Build/Easy Code Web.loader.js
+++ Easy Code Web/Build/Easy Code Web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57926005,
+    wasmFileSize: 57935748,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
